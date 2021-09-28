bc4c03a1fd75ff8901cafe524d3b11da9837ead2
Seth Allgire <seth.allgire@gmail.com>
2021-09-28 13:45:10 -0500

    finalizes app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import { Router } from "react-router";
 import "./App.css";
 import Menu from "./components/Menu";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import SearchPage from "./components/SearchPage";
+import FavoritesPage from "./components/FavoritesPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
+  const [favorites, setFavorites] = useState([]);
+  function addFavorite(gif) {
+    setFavorites((curr) => [...curr, gif]);
+  }
+  function deleteFavorite(id) {
+    setFavorites((curr) => curr.filter((val) => val.id !== id));
+  }
+  const [activeUser, setActiveUser] = useState("");
   return (
     <Router>
       <Menu />
+      <Switch>
+        <Route path="/login">
+          <LoginPage setActiveUser={setActiveUser} />
+        </Route>
+        <Route path="/search">
+          <SearchPage
+            activeUser={activeUser}
+            favorites={favorites}
+            addFavorite={addFavorite}
+            deleteFavorite={deleteFavorite}
+          />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage
+            activeUser={activeUser}
+            favorites={favorites}
+            deleteFavorite={deleteFavorite}
+          />
+        </Route>
+        <Route path="*">
+          <Redirect to="/login"></Redirect>
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/components/LoginPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LoginPage(props) {
+  return <div>Login Page</div>;
+}
