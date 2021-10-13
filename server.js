50cf0ea1ed7d61730a7bad77f1f24750330f438c
Seth Allgire <seth.allgire@gmail.com>
2021-10-13 14:30:53 -0500

    adds server code

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const userRoutes = require("./server/routes/users.routes");
+const favoritesRoutes = require("./server/routes/favorites.routes");
+
+app.use(express.json());
+app.use(express.static(__dirname + "/build"));
+app.use("/users", userRoutes);
+app.use("/favorites", favoritesRoutes);
+
+app.get("*", (req, res) => {
+  return res.sendFile("/build/index.html", { root: __dirname + "/" });
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
